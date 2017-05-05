e36b9a3ce25beabd6854b341eebe82714a13914e
Sabin <spradh@users.noreply.github.com>
2017-05-05 18:27:16 -0400

    Update Call Option Pricing Binomial Tree.m

--- Linear Programming/Call Option Pricing Binomial Tree.m
+++ Linear Programming/Call Option Pricing Binomial Tree.m
@@ -1,24 +1,24 @@
 #Question 1
-T=1/12;               % Expiration in years
-N=5;                  % Number of steps
-delta_t=T/N           % Time step
-r=.02;                % Risk free rate
-sigma=sqrt(.15);      % Volatility
-S0= 144;              % Current Stock Price
-K=150;                % Strike price
-u=e.^(sigma*sqrt(T))  % Uptick Increment
-d=e.^(-sigma*sqrt(T)) % Downtick Decrement
-p=(e.^(r*T)-d)/(u-d)  % Probability of Uptick
-S=[];                 % Maturity stock price vector
-f=zeros(N+1);         % Option value grid
+%Initializing variables
+T=1/12                      % Expiration in years
+N=100                       % Number of steps
+delta_t=T/N                 % Time step
+r=.02                       % Risk free rate
+sigma=sqrt(.15)             % Volatility
+S0= 144                     % Current Stock Price
+K=150                       % Strike price
+u=e.^(sigma*sqrt(delta_t))  % Uptick Increment
+d=e.^(-sigma*sqrt(delta_t)) % Downtick Decrement
+p=(e.^(r*delta_t)-d)/(u-d)  % Probability of Uptick
+S=[];                       % Maturity stock price vector
+f=zeros(N+1);               % Option value grid
 
 
 %Initializing stock price at maturity
 for j=1:N+1,
   S(j)=S0*u^(j-1)*d^(N+1-j);
 end,
-printf('Stock Prices at Maturity: \n')
-S
+
 
 %Initializing option value at maturity
 
@@ -32,7 +32,6 @@ for i=N:-1:1,
     f(j,i)=e.^(-r*delta_t)*(p*f(j,i+1)+(1-p)*f(j+1,i+1));
   end,
 end,
-printf('Option Value Grid : \n')
-f
 
-fprintf('Value of option at current date is $%f.\n\n', f(1,1))
+
+fprintf("\n\nValue of option at current date is $%f.\n\n", f(1,1))
